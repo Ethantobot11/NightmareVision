87d2de11159d4cb3985ccebb1610e65a6ea67c9f
Ethantobot11 <156458071+Ethantobot11@users.noreply.github.com>
2025-07-14 16:11:13 +0200

    unzipgit

--- source/Main.hx
+++ source/Main.hx
@@ -1,261 +1,336 @@
 package;
 
+import online.GameClient;
+import states.MainMenuState;
+import externs.WinAPI;
+import haxe.Exception;
+import flixel.graphics.FlxGraphic;
+import haxe.io.Path;
+import flixel.FlxGame;
+import flixel.FlxState;
+import openfl.Assets;
 import openfl.Lib;
+import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
 import openfl.display.StageScaleMode;
+import lime.app.Application;
+import states.TitleState;
+#if mobile
+import mobile.backend.MobileScaleMode;
+#end
 
-import flixel.FlxG;
-import flixel.FlxGame;
-import flixel.FlxState;
+#if (linux || mac)
+import lime.graphics.Image;
+#end
 
-import funkin.backend.DebugDisplay;
+import backend.io.PsychFileSystem as FileSystem;
 
+#if (linux && !debug)
+@:cppInclude('./external/gamemode_client.h')
+@:cppFileCode('#define GAMEMODE_AUTO')
+#end
 class Main extends Sprite
 {
-	public static final PSYCH_VERSION:String = '0.5.2h';
-	public static final NMV_VERSION:String = '0.2';
-	public static final FUNKIN_VERSION:String = '0.2.7';
-	
-	public static final startMeta =
-		{
-			width: 1280,
-			height: 720,
-			fps: 60,
-			skipSplash: #if debug true #else false #end,
-			startFullScreen: false,
-			initialState: funkin.states.TitleState
-		};
-		
+	public static var game = {
+		width: 1280, // WINDOW width
+		height: 720, // WINDOW height
+		initialState: TitleState, // initial game state
+		zoom: -1.0, // game state bounds
+		framerate: 60, // default framerate
+		skipSplash: true, // if the default flixel splash screen should be skipped
+		startFullscreen: false // if the game should start at fullscreen mode
+	};
+
+	public static var fpsVar:FPS;
+
+	public static final PSYCH_ONLINE_VERSION:String = "0.11.9";
+	public static final CLIENT_PROTOCOL:Float = 8;
+	public static final GIT_COMMIT:String = online.backend.Macros.getGitCommitHash();
+	public static final LOW_STORAGE:Bool = online.backend.Macros.hasNoCapacity();
+	public static var UNOFFICIAL_BUILD:Bool = Main.LOW_STORAGE;
+
+	public static var wankyUpdate:String = 'version';
+	public static var latestRelease:Dynamic = {};
+	public static var updateVersion:String = '';
+
+	public static var view3D:online.away.View3DHandler;
+
 	// You can pretty much ignore everything from here on - your code should go in your states.
-	public static var fpsVar:DebugDisplay;
-	
-	static function __init__()
-	{
-		funkin.utils.MacroUtil.haxeVersionEnforcement();
-	}
-	
+
 	public static function main():Void
 	{
-		Lib.current.addChild(new Main());
-	}
-	
-	public function new()
-	{
-		super();
-		
-		initHaxeUI();
-		
-		#if (windows && cpp)
-		funkin.api.NativeWindows.setDarkMode();
-		#end
-		
-		// load save data before creating FlxGame
-		ClientPrefs.loadDefaultKeys();
-		FlxG.save.bind('funkin', CoolUtil.getSavePath());
-		
-		var game = new
-			#if CRASH_HANDLER
-			FNFGame
-			#else
-			FlxGame
-			#end(startMeta.width, startMeta.height, Init, startMeta.fps, startMeta.fps, true, startMeta.startFullScreen);
-			
-		// FlxG.game._customSoundTray wants just the class, it calls new from
-		// create() in there, which gets called when it's added to stage
-		// which is why it needs to be added before addChild(game) here
-		
-		// Also btw game has to be a variable for this to work ig - Orbyy
-		
-		@:privateAccess
-		game._customSoundTray = funkin.objects.FunkinSoundTray;
-		
-		addChild(game);
-		
-		#if !mobile
-		fpsVar = new DebugDisplay(10, 3, 0xFFFFFF);
-		addChild(fpsVar);
-		Lib.current.stage.align = "tl";
-		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
-		if (fpsVar != null)
-		{
-			fpsVar.visible = ClientPrefs.showFPS;
+		#if !mobile // would crash the game
+		if (Path.normalize(Sys.getCwd()) != Path.normalize(lime.system.System.applicationDirectory)) {
+			Lib.application.window.alert("Your path is either not run from the game directory,\nor contains illegal UTF-8 characters!\n\nRun from: "
+				+ Sys.getCwd()
+				+ "\nExpected path: " + lime.system.System.applicationDirectory, 
+			"Invalid Runtime Path!");
+			Sys.exit(1);
 		}
 		#end
 		
+		Lib.current.addChild(view3D = new online.away.View3DHandler());
+		Lib.current.addChild(new Main());
+		Lib.current.addChild(new online.gui.sidebar.SideUI());
+		Lib.current.addChild(new online.gui.Alert());
+		Lib.current.addChild(new online.gui.LoadingScreen());
+	}
+
+	public function new()
+	{
+		super();
+		#if mobile
+		#if android
+		StorageUtil.requestPermissions();
+		#end
+		Sys.setCwd(StorageUtil.getStorageDirectory());
+		#end
+		backend.CrashHandler.init();
+
+		#if (cpp && windows)
+		backend.Native.fixScaling();
+		#end
+
+		if (stage != null)
+		{
+			init();
+		}
+		else
+		{
+			addEventListener(Event.ADDED_TO_STAGE, init);
+		}
+	}
+
+	private function init(?E:Event):Void
+	{
+		if (hasEventListener(Event.ADDED_TO_STAGE))
+		{
+			removeEventListener(Event.ADDED_TO_STAGE, init);
+		}
+
+		setupGame();
+	}
+
+	private function setupGame():Void
+	{
+		#if (openfl <= "9.2.0")
+		var stageWidth:Int = Lib.current.stage.stageWidth;
+		var stageHeight:Int = Lib.current.stage.stageHeight;
+
+		if (game.zoom == -1.0)
+		{
+			var ratioX:Float = stageWidth / game.width;
+			var ratioY:Float = stageHeight / game.height;
+			game.zoom = Math.min(ratioX, ratioY);
+			game.width = Math.ceil(stageWidth / game.zoom);
+			game.height = Math.ceil(stageHeight / game.zoom);
+		}
+		#else
+		if (game.zoom == -1.0)
+			game.zoom = 1.0;
+		#end
+
+		#if LUA_ALLOWED
+		Mods.pushGlobalMods();
+		#end
+		Mods.loadTopMod();
+
+		CoolUtil.setDarkMode(true);
+
+		#if lumod
+		Lumod.addons.push(online.backend.LuaModuleSwap.LumodModuleAddon);
+		Lumod.scriptPathHandler = scriptPath -> {
+			var defaultPath:String = 'lumod/' + scriptPath;
+
+			// check if script exists in any of loaded mods
+			var path:String = Paths.modFolders(defaultPath);
+			if (FileSystem.exists(path))
+				return path;
+
+			return defaultPath;
+		}
+		Lumod.classResolver = Deflection.resolveClass;
+		Lumod.initializeLuaCallbacks = false;
+		#end
+
+		#if hl
+		sys.ssl.Socket.DEFAULT_VERIFY_CERT = false;
+		#end
+	
+		#if LUA_ALLOWED Lua.set_callbacks_function(cpp.Callable.fromStaticFunction(psychlua.CallbackHandler.call)); #end
+		Controls.instance = new Controls();
+		ClientPrefs.loadDefaultKeys();
+		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
+
+		fpsVar = new FPS(10, 3, 0xFFFFFF);
+		addChild(fpsVar);
+		Lib.current.stage.align = "tl";
+		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
+		if(fpsVar != null) {
+			fpsVar.visible = ClientPrefs.data.showFPS;
+		}
+
+		#if (linux || mac)
+		final icon:Image = Image.fromFile("icon.png");
+		Lib.current.stage.window.setIcon(icon);
+		#end
+
 		#if html5
 		FlxG.autoPause = false;
 		FlxG.mouse.visible = false;
 		#end
-		
-		FlxG.signals.gameResized.add(onResize);
-		
-		#if DISABLE_TRACES
-		haxe.Log.trace = (v:Dynamic, ?infos:haxe.PosInfos) -> {}
+
+		FlxG.fixedTimestep = false;
+		FlxG.game.focusLostFramerate = #if mobile 30 #else 60 #end;
+		#if web
+		FlxG.keys.preventDefaultKeys.push(TAB);
+		#else
+		FlxG.keys.preventDefaultKeys = [TAB];
 		#end
-	}
-	
-	@:access(flixel.FlxCamera)
-	static function onResize(w:Int, h:Int)
-	{
-		final scale:Float = Math.max(1, Math.min(w / FlxG.width, h / FlxG.height));
-		if (fpsVar != null)
-		{
-			// remove for now idfk some people have the textfield scaling fucky ???
-			// fpsVar.scaleX = fpsVar.scaleY = scale;
-		}
-		
-		if (FlxG.cameras != null)
-		{
-			for (i in FlxG.cameras.list)
-			{
-				if (i != null && i.filters != null) resetSpriteCache(i.flashSprite);
+
+		#if android FlxG.android.preventDefaultKeys = [BACK]; #end
+
+		#if DISCORD_ALLOWED
+		DiscordClient.start();
+		#end
+
+		#if mobile
+		lime.system.System.allowScreenTimeout = ClientPrefs.data.screensaver; 		
+		FlxG.scaleMode = new MobileScaleMode();
+		#end
+
+		Application.current.window.vsync = ClientPrefs.data.vsync;
+
+		// shader coords fix
+		FlxG.signals.gameResized.add(function (w, h) {
+			if(fpsVar != null)
+				fpsVar.positionFPS(10, 3, Math.min(w / FlxG.width, h / FlxG.height));
+
+		     if (FlxG.cameras != null) {
+			   for (cam in FlxG.cameras.list) {
+				@:privateAccess
+				if (cam != null && cam.filters != null)
+					resetSpriteCache(cam.flashSprite);
+			   }
+		     }
+
+		     if (FlxG.game != null)
+			 resetSpriteCache(FlxG.game);
+		});
+
+		//ONLINE STUFF, BELOW CODE USE FOR BACKPORTING
+
+		var http = new haxe.Http("https://raw.githubusercontent.com/Snirozu/Funkin-Psych-Online/main/server_addresses.txt");
+		http.onData = function(data:String) {
+			for (address in data.split(',')) {
+				online.GameClient.serverAddresses.push(address.trim());
 			}
 		}
-		
-		if (FlxG.game != null)
-		{
-			resetSpriteCache(FlxG.game);
+		http.onError = function(error) {
+			trace('error: $error');
 		}
+		http.request();
+		#if LOCAL
+		online.GameClient.serverAddresses.insert(0, "ws://localhost:2567");
+		#else
+		online.GameClient.serverAddresses.push("ws://localhost:2567");
+		#end
+		online.network.FunkinNetwork.client = new online.http.HTTPHandler(online.GameClient.addressToUrl());
+
+		online.mods.ModDownloader.checkDeleteDlDir();
+
+		addChild(new online.gui.DownloadAlert.DownloadAlerts());
+
+		FlxG.plugins.add(new online.backend.Waiter());
+
+		online.backend.Thread.repeat(() -> {
+			try {
+				online.network.FunkinNetwork.ping();
+			}
+			catch (exc) {
+				trace(exc);
+			}
+		}, 60, _ -> {}); // ping the server every minute
+		
+		//for some reason only cancels 2 downloads
+		Lib.application.window.onClose.add(() -> {
+			#if DISCORD_ALLOWED
+			DiscordClient.shutdown();
+			#end
+			online.mods.ModDownloader.cancelAll();
+			online.mods.ModDownloader.checkDeleteDlDir();
+			online.network.Auth.saveClose();
+		});
+
+		#if !mobile
+		Lib.application.window.onDropFile.add(path -> {
+			if (FileSystem.isDirectory(path))
+				return;
+
+			if (path.endsWith(".json") && (path.contains("-chart") || path.contains("-metadata"))) {
+				online.util.vslice.VUtil.convertVSlice(path);
+			}
+			else {
+				online.backend.Thread.run(() -> {
+					online.gui.LoadingScreen.toggle(true);
+					online.mods.OnlineMods.installMod(path);
+					online.gui.LoadingScreen.toggle(false);
+				});
+			}
+		});
+		#end
+
+		// clear messages before the current state gets destroyed and replaced with another
+		FlxG.signals.preStateSwitch.add(() -> {
+			GameClient.clearOnMessage();
+		});
+
+		FlxG.signals.postGameReset.add(() -> {
+			online.gui.Alert.alert('Warning!', 'The game has been resetted, and there may occur visual bugs with the sidebar!\n\nIt\'s recommended to restart the game instead.');
+		});
+		
+		#if HSCRIPT_ALLOWED
+		FlxG.signals.postStateSwitch.add(() -> {
+			online.backend.SyncScript.dispatch("switchState", [FlxG.state]);
+
+			FlxG.state.subStateOpened.add(substate -> {
+				online.backend.SyncScript.dispatch("openSubState", [substate]);
+			});
+		});
+
+		FlxG.signals.postUpdate.add(() -> {
+			if (online.backend.SyncScript.activeUpdate)
+				online.backend.SyncScript.dispatch("update", [FlxG.elapsed]);
+		});
+
+		online.backend.SyncScript.resyncScript(false, () -> {
+			online.backend.SyncScript.dispatch("init");
+		});
+		#end
 	}
-	
-	public static function resetSpriteCache(sprite:Sprite):Void
-	{
-		@:privateAccess
-		{
-			sprite.__cacheBitmap = null;
+
+	static function resetSpriteCache(sprite:Sprite):Void {
+		@:privateAccess {
+		        sprite.__cacheBitmap = null;
 			sprite.__cacheBitmapData = null;
 		}
 	}
-	
-	function initHaxeUI():Void
-	{
-		#if haxeui_core
-		haxe.ui.Toolkit.init();
-		haxe.ui.Toolkit.theme = 'dark';
-		haxe.ui.Toolkit.autoScale = false;
-		haxe.ui.focus.FocusManager.instance.autoFocus = false;
-		haxe.ui.tooltips.ToolTipManager.defaultDelay = 200;
-		#end
-	}
-}
 
-#if CRASH_HANDLER
-class FNFGame extends FlxGame
-{
-	private static function crashGame()
-	{
-		null
-		.draw();
-	}
-	
-	/**
-	 * Used to instantiate the guts of the flixel game object once we have a valid reference to the root.
-	 */
-	override function create(_):Void
-	{
-		try
-		{
-			_skipSplash = true;
-			super.create(_);
-		}
-		catch (e)
-		{
-			onCrash(e);
-		}
-	}
-	
-	override function onFocus(_):Void
-	{
-		try
-		{
-			super.onFocus(_);
-		}
-		catch (e)
-		{
-			onCrash(e);
-		}
-	}
-	
-	override function onFocusLost(_):Void
-	{
-		try
-		{
-			super.onFocusLost(_);
-		}
-		catch (e)
-		{
-			onCrash(e);
-		}
-	}
-	
-	/**
-	 * Handles the `onEnterFrame` call and figures out how many updates and draw calls to do.
-	 */
-	override function onEnterFrame(_):Void
-	{
-		try
-		{
-			super.onEnterFrame(_);
-		}
-		catch (e)
-		{
-			onCrash(e);
-		}
-	}
-	
-	/**
-	 * This function is called by `step()` and updates the actual game state.
-	 * May be called multiple times per "frame" or draw call.
-	 */
-	override function update():Void
-	{
-		#if CRASH_TEST
-		if (FlxG.keys.justPressed.F9) crashGame();
+	public static function getTime():Float {
+		#if flash
+		return flash.Lib.getTimer();
+		#elseif ((js && !nodejs) || electron)
+		return js.Browser.window.performance.now();
+		#elseif sys
+		return Sys.time() * 1000;
+		#elseif (lime_cffi && !macro)
+		@:privateAccess
+		return cast lime._internal.backend.native.NativeCFFI.lime_system_get_timer();
+		#elseif cpp
+		return untyped __global__.__time_stamp() * 1000;
+		#else
+		return 0;
 		#end
-		try
-		{
-			super.update();
-		}
-		catch (e)
-		{
-			onCrash(e);
-		}
-	}
-	
-	/**
-	 * Goes through the game state and draws all the game objects and special effects.
-	 */
-	override function draw():Void
-	{
-		try
-		{
-			super.draw();
-		}
-		catch (e)
-		{
-			onCrash(e);
-		}
-	}
-	
-	private final function onCrash(e:haxe.Exception):Void
-	{
-		var emsg:String = "";
-		for (stackItem in haxe.CallStack.exceptionStack(true))
-		{
-			switch (stackItem)
-			{
-				case FilePos(s, file, line, column):
-					emsg += file + " (line " + line + ")\n";
-				default:
-					Sys.println(stackItem);
-					trace(stackItem);
-			}
-		}
-		
-		final crashReport = 'Error caught:' + e.message + '\nCallstack:\n' + emsg;
-		
-		FlxG.switchState(new funkin.backend.FallbackState(crashReport, () -> FlxG.switchState(() -> new MainMenuState())));
 	}
 }
-#end
