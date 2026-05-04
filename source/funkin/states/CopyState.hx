f080e17bebc55577c579590c7ce3020fc9644dba
Ethantobot <ethantobot7@gmail.com>
2026-05-04 16:38:26 +0200

    Update CopyState.hx

--- source/funkin/states/CopyState.hx
+++ source/funkin/states/CopyState.hx
@@ -62,7 +62,7 @@ class CopyState extends MusicBeatState
 		checkExistingFiles();
 		if (maxLoopTimes <= 0)
 		{
-			MusicBeatState.switchState(new TitleState());
+			FlxG.switchState(funkin.states.TitleState.new()); le
 			return;
 		}
 
