1aaf380285f9aac5040e3579a04432c4528511ea
Ethantobot <ethantobot7@gmail.com>
2026-05-08 14:33:53 +0200

    Update Memory.hx

--- source/external/memory/Memory.hx
+++ source/external/memory/Memory.hx
@@ -1,11 +1,10 @@
 package external.memory;
 
-#if cpp
+// Only allow this class to exist on platforms where we have the C++ header ready
+#if (cpp && (windows || android || ios))
+
 /**
  * Memory class to properly get accurate memory counts
- * for the program.
- * @author Leather128 (Haxe Bindings) - David Robert Nadeau (Original C Header)
- * even if the author is above this, thank you Leather128 for the Haxe Bindings!
  */
 #if windows
 @:buildXml('<include name="../../../../source/external/memory/build.xml" />')
@@ -23,5 +22,15 @@ extern class Memory
 	 */
 	@:native("getCurrentRSS")
 	public static function getCurrentUsage():Float;
+}
+
+#else
+
+// Fallback class for Mac, Linux, and others so the code doesn't crash
+class Memory 
+{
+	public static function getCurrentUsage():Float {
+		return 0.0;
+	}
 }
 #end
